6da70e22354ac4bca95b6c0c04ea0925a768d435
Liam Black <liamblack333@gmail.com>
2024-07-17 12:56:00 +0100

    Update import

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { Resend } from "resend"; // Import Resend library
+import { Resend } from "resend";
 
 const DetailsPage = () => {
   const [savedCustomization, setSavedCustomization] = useState<any>(null);
