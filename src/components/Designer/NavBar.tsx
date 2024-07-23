9697d6ab2c22604801245beecd074cc3a8fcf475
Liam Black <liamblack333@gmail.com>
2024-07-24 00:44:56 +0100

    add seo improvements

--- src/components/Designer/NavBar.tsx
+++ src/components/Designer/NavBar.tsx
@@ -1,47 +1,10 @@
-"use client";
+// components/Navbar.js
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
-
-interface Menu {
-  title: string;
-  titleHref: string;
-  links: { href: string; label: string }[];
-}
 
 const Navbar = () => {
-  const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
-
-  const handleMenuEnter = (menu: string) => {
-    setActiveDropdown(menu);
-  };
-
-  const handleMenuLeave = () => {
-    setActiveDropdown(null);
-  };
-
-  const handleDropdownEnter = (menu: string) => {
-    setActiveDropdown(menu);
-  };
-
-  const handleDropdownLeave = () => {
-    setActiveDropdown(null);
-  };
-
-  const menus: Menu[] = [
-    {
-      title: "Help",
-      titleHref: "/about",
-      links: [
-        { href: "/about", label: "About Us" },
-        { href: "/contact", label: "Contact Us" },
-        { href: "/faqs", label: "FAQs" },
-      ],
-    },
-  ];
-
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative z-50 bg-[#F5F7FA]">
+    <header className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative z-50 bg-[#F5F7FA]">
       {/* MOBILE */}
       <div className="h-full flex items-center justify-center md:hidden">
         <Link href="/" className="flex items-center gap-3">
@@ -52,10 +15,9 @@ const Navbar = () => {
             height={60}
           />
         </Link>
-        {/* <Menu /> */}
       </div>
       {/* BIGGER SCREENS */}
-      <div className="hidden md:flex items-center justify-between gap-8 h-full z-50 bg-[#F5F7FA]">
+      <nav className="hidden md:flex items-center justify-between gap-8 h-full z-50 bg-[#F5F7FA]">
         {/* LEFT */}
         <div className="flex items-center gap-12"></div>
         {/* CENTER */}
@@ -69,8 +31,8 @@ const Navbar = () => {
             />
           </Link>
         </div>
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 };
 
