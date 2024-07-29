e5dbfca3dc2e7dc6145e4218f5ed0974fccd4223
Liam Black <liamblack333@gmail.com>
2024-07-29 20:08:38 +0100

    adjust sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,10 +10,22 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://www.gripgear.co.uk/details",
+      url: "https://www.gripgear.co.uk/design",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
+    },
+    {
+      url: "https://www.gripgear.co.uk/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://www.gripgear.co.uk/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
     },
   ];
 }
