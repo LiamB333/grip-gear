5c0b22cd265ce6aef8f90e55865fc6611795b317
Liam Black <liamblack333@gmail.com>
2024-08-03 22:10:37 +0100

    remove button styling

--- src/components/Website/CustomiseAction/Button.tsx
+++ src/components/Website/CustomiseAction/Button.tsx
@@ -9,7 +9,7 @@ export const Button: React.FC<ButtonProps> = ({ text }) => {
   return (
     <div className="flex flex-col justify-center my-auto text-2xl font-medium leading-9 text-white">
       <Link href="/design">
-        <button className="px-6 py-3 bg-[#C62828] transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-[#911d1d] duration-300 rounded-[30px] cursor-pointer max-md:px-5">
+        <button className="px-6 py-3 bg-[#C62828] hover:bg-[#911d1d] rounded-[30px] cursor-pointer max-md:px-5">
           {text}
         </button>
       </Link>
